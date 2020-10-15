e3a97ea526a2b160375bdc2df1268d9c0133886e
AahzBrut <partyomenko@gmail.com>
2020-10-15 08:56:50 +0300

    Remove all functionality which allowed to modify roles

--- src/main/kotlin/io/github/recipestore/controller/RoleController.kt
+++ src/main/kotlin/io/github/recipestore/controller/RoleController.kt
@@ -1,18 +1,12 @@
 package io.github.recipestore.controller
 
 import io.github.recipestore.domain.Role
-import io.github.recipestore.dto.request.RoleAddRequest
-import io.github.recipestore.dto.request.RoleUpdateRequest
 import io.github.recipestore.service.RoleService
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
@@ -46,29 +40,4 @@ class RoleController(
         .getRoleById(id)
         .doOnSubscribe { logger.debug("User: ${principal.name} in getRoleById()") }
         .doAfterTerminate { logger.debug("User: ${principal.name} out of getRoleById()") }
-
-    @ResponseStatus(HttpStatus.OK)
-    @PostMapping(ROLES_PATH,
-        consumes = [APPLICATION_JSON_VALUE],
-        produces = [APPLICATION_JSON_VALUE])
-    fun addRole(principal: Principal, @RequestBody request: RoleAddRequest): Mono<Role> = roleService
-        .addRole(request)
-        .doOnSubscribe { logger.debug("User: ${principal.name} in addRole()") }
-        .doAfterTerminate { logger.debug("User: ${principal.name} out of addRole()") }
-
-    @ResponseStatus(HttpStatus.OK)
-    @PutMapping(ROLES_PATH,
-        consumes = [APPLICATION_JSON_VALUE],
-        produces = [APPLICATION_JSON_VALUE])
-    fun updateRole(principal: Principal, @RequestBody request: RoleUpdateRequest): Mono<Role> = roleService
-        .updateRole(request)
-        .doOnSubscribe { logger.debug("User: ${principal.name} in updateRole()") }
-        .doAfterTerminate { logger.debug("User: ${principal.name} out of updateRole()") }
-
-    @ResponseStatus(HttpStatus.OK)
-    @DeleteMapping(ROLES_BY_ID_PATH)
-    fun deleteRole(principal: Principal, @PathVariable id: Long): Mono<Void> = roleService
-        .deleteRole(id)
-        .doOnSubscribe { logger.debug("User: ${principal.name} in deleteRole()") }
-        .doAfterTerminate { logger.debug("User: ${principal.name} out of deleteRole()") }
 }
--- src/main/kotlin/io/github/recipestore/service/RoleService.kt
+++ src/main/kotlin/io/github/recipestore/service/RoleService.kt
@@ -1,8 +1,6 @@
 package io.github.recipestore.service
 
 import io.github.recipestore.domain.Role
-import io.github.recipestore.dto.request.RoleAddRequest
-import io.github.recipestore.dto.request.RoleUpdateRequest
 import io.github.recipestore.repository.RoleRepository
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
@@ -18,16 +16,4 @@ class RoleService(
 
     fun getAllRoles(): Flux<Role> =
         roleRepository.findAll()
-
-    fun addRole(request: RoleAddRequest): Mono<Role> = Mono
-        .just(request)
-        .map { Role(null, it.name, it.description) }
-        .flatMap { roleRepository.save(it) }
-
-    fun updateRole(request: RoleUpdateRequest): Mono<Role> = Mono
-        .just(request)
-        .map { Role(request.id, request.name, request.description) }
-        .flatMap { roleRepository.save(it) }
-
-    fun deleteRole(id: Long): Mono<Void> = roleRepository.deleteById(id)
 }
