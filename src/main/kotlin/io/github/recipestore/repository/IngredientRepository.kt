f97f66f12cdfe2d6695a86a63cd0f3949943eb69
AahzBrut <partyomenko@gmail.com>
2020-10-17 10:51:12 +0300

    Add ingredients and user to ingredient category response
    closes #13

--- src/main/kotlin/io/github/recipestore/repository/IngredientRepository.kt
+++ src/main/kotlin/io/github/recipestore/repository/IngredientRepository.kt
@@ -4,9 +4,12 @@ import io.github.recipestore.domain.Ingredient
 import org.springframework.data.r2dbc.repository.Modifying
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface IngredientRepository : ReactiveCrudRepository<Ingredient, Long> {
+
+    fun findAllByCategoryId(categoryId: Long): Flux<Ingredient>
 
     @Modifying
     @Query("UPDATE REPOSITORY.INGREDIENT SET NAME=:name, DESCRIPTION=:description, INGREDIENT_CATEGORY_ID=:categoryId, USER_ID=:userId, MODIFIED=CURRENT_TIMESTAMP() WHERE INGREDIENT_ID=:id")
