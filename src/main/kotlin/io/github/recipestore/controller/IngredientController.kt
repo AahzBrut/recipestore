953f133c2f03ae17b4577053e9aac37871bc9a6f
Павел Артеменко <partyomenko@gmail.com>
2020-11-07 16:00:21 +0300

    Merge pull request #31
    * Add endpoint to upload images of ingredients

--- src/main/kotlin/io/github/recipestore/controller/IngredientController.kt
+++ src/main/kotlin/io/github/recipestore/controller/IngredientController.kt
@@ -5,12 +5,14 @@ import io.github.recipestore.dto.request.IngredientRequest
 import io.github.recipestore.service.IngredientService
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.codec.multipart.FilePart
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
@@ -20,12 +22,12 @@ import java.security.Principal
 
 const val INGREDIENTS_PATH = "/api/v1/ingredients"
 const val INGREDIENTS_BY_ID_PATH = "/api/v1/ingredients/{id}"
+const val INGREDIENTS_IMAGE_PATH = "/api/v1/ingredients/{id}/image"
 
 @RestController
 class IngredientController(
     private val service: IngredientService
 ) {
-
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping(INGREDIENTS_PATH,
@@ -58,4 +60,10 @@ class IngredientController(
     fun deleteIngredient(principal: Principal, @PathVariable id: Long): Mono<Void> =
         service
             .deleteIngredient(id)
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping(INGREDIENTS_IMAGE_PATH,
+        consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun setImage(principal: Principal, @PathVariable id: Long, @RequestPart("image") image: Mono<FilePart>): Mono<Void> =
+        service.setImage(id, image)
 }
